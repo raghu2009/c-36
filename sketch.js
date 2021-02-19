6020a39920b0f1fe4043b47a6352319d434070e6
raghu2009 <74768710+raghu2009@users.noreply.github.com>
2021-02-19 15:00:33 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,24 @@
+var backgroundimage
+var gameState=0
+var playerCount
+var database
+var form,player,game
+
+
+function setup(){
+  database = firebase.database();
+  
+  createCanvas(500,500);
+  game=new Game();
+  game.getState();
+  game.start();
+
+
+  
+}
+
+function draw(){
+  
+  
+}
+
